2cdc68048f49c8aefec31aa38c7148937bbe581b
Tim Smith <tsmith@llnw.com>
2013-01-15 10:02:47 -0800

    Switch to platform family, add back the long_description, add oracle linux support

--- collectd/metadata.rb
+++ collectd/metadata.rb
@@ -1,10 +1,11 @@
 name             "collectd"
 maintainer       "Noan Kantrowitz"
 maintainer_email "nkantrowitz@crypticstudios.com"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 license          "Apache 2.0"
 description      "Install and configure the collectd monitoring daemon"
 version          "1.0.2"
 
-%w{ debian ubuntu centos redhat amazon scientific fedora}.each do |os|
+%w{ debian ubuntu centos redhat amazon oracle scientific fedora}.each do |os|
   supports os
 end
