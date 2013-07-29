8246bf59ed96823c38e35d7ae7e85c2f810dbdef
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-07-29 11:03:56 -0700

    Removing storm cookbook into it's own repo

--- storm/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'storm'
-maintainer       "Webtrends, Inc."
-maintainer_email "kendrick.martin@webtrends.com"
-license          "All rights reserved"
-description      "Installs Twitter's Storm distributed computation system"
-version          "1.2.5"
-depends          "java"
-depends          "runit"
-supports         "ubuntu"
