4746d81da9d1ffb991c61b945312e35287346499
Raymond L. Colanero <rcolanero@mdsol.com>
2013-08-01 15:28:35 -0400

    bumped cookbook version

--- ms_dotnet45/metadata.rb
+++ ms_dotnet45/metadata.rb
@@ -4,6 +4,6 @@ license           "Apache 2.0"
 license          "All rights reserved"
 description      "Installs/Configures Microsoft .NET 4.5"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 supports	       "windows"
 depends          "windows"
