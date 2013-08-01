7026905afda3d14c8f901dd9d9afa63554cc8ebd
Raymond L. Colanero <rcolanero@mdsol.com>
2013-08-01 15:13:50 -0400

    updated ms_dotnet4 and ms_dotnet45 cookbooks with timeout attribute

--- ms_dotnet4/attributes/defaults.rb
+++ ms_dotnet4/attributes/defaults.rb
@@ -19,3 +19,4 @@
 #
 
 default['ms_dotnet4']['http_url'] = "http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe"
+default['ms_dotnet4']['timeout'] = 600
